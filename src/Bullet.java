3ba9d34d573ac0445c49e82081d8c13e71fd7d43
Daniil Kerna <danilkak101@gmail.com>
2018-11-28 02:05:12 -0800

    Add the Ability to Shoot

---
+++ src/Bullet.java
@@ -0,0 +1,93 @@
+import jig.Entity;
+import jig.Vector;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Bullet extends Entity {
+    private static float DEFAULT_BULLET_VELOCITY = 0.25f;
+
+    private Vector      bulletVelocity;
+    private BulletType  bulletType;
+
+    public Bullet (final float x, final float y , BulletType bulletType , PlayerHorizontalDirection playerHorizontalDirection,
+                   PlayerVerticalDirection playerVerticalDirection, PlayerState playerState , PlayerMovement playerMovement){
+        super( x , y - 25);
+
+        this.setScale(2f);
+        this.bulletType = bulletType;
+
+        if (bulletType == BulletType.REGULAR)
+            addImageWithBoundingBox(ContraGame.getImageAsset("BULLET_REGULAR"));
+        else{
+            addImageWithBoundingBox(ContraGame.getImageAsset("BULLET_UPGRADE"));
+        }
+
+        if (playerVerticalDirection == PlayerVerticalDirection.UP ){
+            //shoot straight up
+            if (playerMovement == PlayerMovement.NONE)
+                this.bulletVelocity = new Vector(0 , -DEFAULT_BULLET_VELOCITY);
+
+            //shoot up and right
+            else if (playerHorizontalDirection == PlayerHorizontalDirection.RIGHT )
+                this.bulletVelocity = new Vector(DEFAULT_BULLET_VELOCITY , -DEFAULT_BULLET_VELOCITY);
+            //shoot up and left
+            else if (playerHorizontalDirection == PlayerHorizontalDirection.LEFT )
+                this.bulletVelocity = new Vector(-DEFAULT_BULLET_VELOCITY , -DEFAULT_BULLET_VELOCITY);
+            //shot up
+            else
+                this.bulletVelocity = new Vector(0 , -DEFAULT_BULLET_VELOCITY);
+        }
+        else if (playerVerticalDirection == PlayerVerticalDirection.NONE){
+            //shoot straight left
+            if (playerHorizontalDirection == PlayerHorizontalDirection.LEFT)
+                this.bulletVelocity = new Vector(-DEFAULT_BULLET_VELOCITY , 0);
+            //shoot straight right
+            else
+                this.bulletVelocity = new Vector(DEFAULT_BULLET_VELOCITY , 0);
+
+        }
+        else if (playerVerticalDirection == PlayerVerticalDirection.DOWN){
+
+            if (playerState == PlayerState.RUNNING || playerState == PlayerState.JUMPING) {
+                //shoot straight down
+                if (playerMovement == PlayerMovement.NONE)
+                    this.bulletVelocity = new Vector(0, DEFAULT_BULLET_VELOCITY);
+
+                //shoot down and left
+                else if (playerHorizontalDirection == PlayerHorizontalDirection.LEFT)
+                    this.bulletVelocity = new Vector(-DEFAULT_BULLET_VELOCITY, DEFAULT_BULLET_VELOCITY);
+
+                //shoot down and right
+                else
+                    this.bulletVelocity = new Vector(DEFAULT_BULLET_VELOCITY, DEFAULT_BULLET_VELOCITY);
+
+            }
+                //else prone
+            else {
+                setPosition( x , y - 6);
+                //shoot straight left
+                if (playerHorizontalDirection == PlayerHorizontalDirection.LEFT)
+                    this.bulletVelocity = new Vector(-DEFAULT_BULLET_VELOCITY, 0);
+                    //shoot straight right
+                else
+                    this.bulletVelocity = new Vector(DEFAULT_BULLET_VELOCITY, 0);
+            }
+
+        }
+
+        else{
+            this.bulletVelocity = new Vector(DEFAULT_BULLET_VELOCITY , 0);
+        }
+    }
+
+
+
+    public void update(GameContainer gc, StateBasedGame sbg, int delta) {
+
+        setPosition( getX() + bulletVelocity.getX()*delta, getY() + bulletVelocity.getY()*delta );
+
+    }
+
+
+
+}
